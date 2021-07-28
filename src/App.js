b32e1eb1d15c3b2a7ab2a3d27a803d1ef0910445
boscrypter <boboyakacha@gmail.com>
2021-07-27 17:46:23 -0700

    components part1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+<Menu />
 
 class App extends Component {
   render() {
